63df3f54c15af6a3e2b02e5d5e92122f78ffb396
James Hagerman <james.hagerman@gmail.com>
2017-10-08 02:39:54 -0700

    Starting to get the new readFloat, readComplex, countFloat, and countComplex methods working. Not yet handling mantissa bits...

--- app/components/SDR.js
+++ app/components/SDR.js
@@ -25,6 +25,11 @@ export default class SDR extends Component {
     this.updateCanvas = this.updateCanvas.bind(this)
 
     this.handleData = this.handleData.bind(this)
+    this.readFloat = this.readFloat.bind(this)
+    this.countFloats = this.countFloats.bind(this)
+
+    this.readComplex = this.readComplex.bind(this)
+    this.countComplex = this.countComplex.bind(this)
   }
 
   componentDidMount() {
@@ -45,8 +50,8 @@ export default class SDR extends Component {
     // Thus, a value of -1 will send these bytes, in order:
     // 0x0
     // 0x0
-    // 0x128 = 0b10000000 (exponent LSB and 7 mantissa bits)
-    // 0x191 = 0b10111111 (sign and 7 exponent bits)
+    // 0x80 (128) = 0b10000000 (exponent LSB and 7 of 23 mantissa bits)
+    // 0xBF (191) = 0b10111111 (sign and 7 exponent bits)
     //
     // Thus, 32 bit value of -1 is:
     // 0b10111111 10000000 00000000 00000000
@@ -58,12 +63,12 @@ export default class SDR extends Component {
     // Thus, a value of (-1 + 1j) will send these bytes, in order:
     // 0x0
     // 0x0
-    // 0x128
-    // 0x191
+    // 0x80 (128)
+    // 0xBF (191)
     // 0x0
     // 0x0
-    // 0x128
-    // 0x63
+    // 0x80 (128)
+    // 0x3F (63)
     //
     let byteString = ''
     let bytes = []
@@ -82,6 +87,53 @@ export default class SDR extends Component {
 
     // Only let one packet come through (so we don't flood the console)
     this.stopListening()
+  }
+
+  readFloat(buffer, offset) {
+    // Note: We could use the ieee754 NPM module, but it calculates a lot
+    // every time that libraries read() method is called. So we probably
+    // can speed it up a bit by doing the convesion differently.
+    // This code was built while looking at that modules source, but it is
+    // significantly different in it's implementation
+
+    // Floating point format settings:
+    let mLen = 27; // mantissa length
+    let nBytes = 4; // number of bytes in floating point value
+    let isLE = true; // set endianness
+
+    // Note: offset is offset of FLOATS in the buffer. Not bytes!
+    offset = offset * nBytes
+
+    // Work variables:
+    let i = isLE ? (nBytes - 1) : 0
+    let exponentHigh = ((buffer[offset + i] & 0x7F) << 1)
+    let exponentLow = ((buffer[offset + i - 1] & 0x80) >> 7)
+    let exponent = exponentHigh | exponentLow
+    let sign =  (buffer[offset + i] >> 7) ? -1 : 1
+
+    // return [offset, i, sign, exponentHigh, exponentLow, exponent]
+    return exponent * sign
+  }
+
+  readComplex(buffer, offset) {
+    // Note: This will return an array:
+    // Index 0 is the Real part
+    // Index 1 is the Imaginary part
+
+    let nBytes = 8; // number of bytes in a complex point value
+
+    // Note: offset is offset of COMPLEX values in the buffer. Not bytes!
+    offset = offset * nBytes
+
+    return [this.readFloat(buffer, offset), this.readFloat(buffer, offset+1)]
+  }
+
+  countFloats(buffer) {
+    return buffer.length/4
+  }
+
+  countComplex(buffer) {
+    return buffer.length/8
   }
 
   startListening() {
@@ -121,6 +173,16 @@ export default class SDR extends Component {
   }
 
   updateCanvas() {
+    // Test the float and complex data parser APIs:
+    let buffer = [0, 0, 128, 191, 0, 0, 128, 63]
+    console.log('float count:', this.countFloats(buffer))
+    console.log('testing readFloat:', this.readFloat(buffer, 0), this.readFloat(buffer, 1))
+
+    // Treat that same buffer as a complex value:
+    console.log('complex count:', this.countComplex(buffer))
+    console.log('readComplex from buffer:', this.readComplex(buffer, 0))
+
+
     const canvas = this.refs.canvas
     canvas.width  = window.innerWidth - 16
     canvas.height = window.innerHeight - 16
