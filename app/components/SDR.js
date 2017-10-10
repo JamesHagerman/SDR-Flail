461ff6834ff3b8debf363566b007bac6dac44436
James Hagerman <james.hagerman@gmail.com>
2017-10-10 03:20:55 -0700

    Pulling out my bogus ieee float32 nonesense and using the builtin functions for that. Unfortunately, we are network bound and dropping packets. so we need to rework this.

--- app/components/SDR.js
+++ app/components/SDR.js
@@ -11,6 +11,8 @@ let canvasStyle = {
   zIndex: -1
 }
 
+let currentYPos = 0
+
 export default class SDR extends Component {
   constructor(props) {
     super(props)
@@ -20,19 +22,20 @@ export default class SDR extends Component {
       receivedCount: 0,
       rfData: []
     }
+
     this.startListening = this.startListening.bind(this)
     this.stopListening = this.stopListening.bind(this)
     this.updateCanvas = this.updateCanvas.bind(this)
 
     this.handleData = this.handleData.bind(this)
-    this.readFloat = this.readFloat.bind(this)
-    this.countFloats = this.countFloats.bind(this)
-
-    this.readComplex = this.readComplex.bind(this)
-    this.countComplex = this.countComplex.bind(this)
   }
 
   componentDidMount() {
+    this.canvas = this.refs.canvas
+    this.canvas.width  = window.innerWidth - 16
+    this.canvas.height = window.innerHeight - 16
+    this.ctx = this.canvas.getContext('2d')
+
     this.updateCanvas()
   }
 
@@ -43,121 +46,30 @@ export default class SDR extends Component {
   handleData(msg, rinfo) {
     // Floating point numbers:
     // GNU Radio uses IEEE-754 Floating point for it's binary float format
-    // 1 bit Sign
-    // 8 bits Exponent
-    // 23 bits Mantissa
-    //
-    // Thus, a value of -1 will send these bytes, in order:
-    // 0x0
-    // 0x0
-    // 0x80 (128) = 0b10000000 (exponent LSB and 7 of 23 mantissa bits)
-    // 0xBF (191) = 0b10111111 (sign and 7 exponent bits)
-    //
-    // Thus, 32 bit value of -1 is:
-    // 0b10111111 10000000 00000000 00000000
     //
     // Complex numbers:
-    // Apparently, it's just two floats. One for real (sent first), one for
+    // GNU Radio uses 2 IEEE-754 floats for complex values. One for real (sent first), one for
     // imaginary (sent second)
     //
-    // Thus, a value of (-1 + 1j) will send these bytes, in order:
-    // 0x0
-    // 0x0
-    // 0x80 (128)
-    // 0xBF (191)
-    // 0x0
-    // 0x0
-    // 0x80 (128)
-    // 0x3F (63)
-    //
-    let byteString = ''
-    let bytes = []
-    let empty = true
+    // TODO: This method should probably handle multiple data types...
+    let rfData = []
 
-    // Convert the dgram's msg Buffer object to an ArrayBuffer:
-    var ab = new ArrayBuffer(msg.length);
+    // Use Buffer.readFloatLE() to parse a float off of the buffer:
+    // console.warn('oh man i am dumb', msg.readFloatLE(0))
 
-    // Build a DataView on that ArrayBuffer
-    // var view = new Uint8Array(ab);
-    let view = new DataView(ab);
-    for (var i = 0; i < msg.length; ++i) {
-        view[i] = msg[i];
-    }
-
-    // Use ArrayBuffer.getFloat32() to parse a float off of the buffer:
-    console.warn('oh man i am dumb', view.getFloat32(0))
-
-    for (let i = 0; i < msg.length; i++) {
-      if (empty) {
-        empty = (msg[i] === 0)
+    if (msg.length !== 0) {
+      for (let i = 0; i < msg.length/4; i+=4) {
+        rfData.push(msg.readFloatLE(i))
       }
-      byteString = byteString + msg[i].toString(16) + ' '
-      bytes.push(msg[i])
-    }
-    if (!empty) {
-      console.log(`length: ${msg.length} data: ${byteString}`)
-      this.setState({ receivedCount: this.state.receivedCount + 1, rfData: bytes })
+      // console.log(`length: ${msg.length} rfData: ${rfData} rfData.length: ${rfData.length}`)
+      this.setState({ receivedCount: this.state.receivedCount + 1, rfData: rfData })
     }
 
-    // Only let one packet come through (so we don't flood the console)
-    this.stopListening()
+    // Debug: Only let one packet come through (so we don't flood the console)
+    // this.stopListening()
   }
 
-  readFloat(buffer, offset) {
-    // Note: We could use the ieee754 NPM module, but it calculates a lot
-    // every time that libraries read() method is called. So we probably
-    // can speed it up a bit by doing the convesion differently.
-    // This code was built while looking at that modules source, but it is
-    // significantly different in it's implementation
 
-    // Floating point format settings:
-    let mLen = 27; // mantissa length
-    let nBytes = 4; // number of bytes in floating point value
-    let isLE = true; // set endianness
-
-    // Note: offset is offset of FLOATS in the buffer. Not bytes!
-    offset = offset * nBytes
-
-    // Work variables:
-    let i = isLE ? (nBytes - 1) : 0
-    // let sign =  (buffer[offset + i] >> 7) ? -1 : 1 // Convert sign bit using logic
-    let sign =  Math.pow(-1, (buffer[offset + i] >> 7)) // Convert sign bit using math (faster?)
-
-    let exponentHigh = ((buffer[offset + i] & 0x7F) << 1)
-    let exponentLow = ((buffer[offset + i - 1] & 0x80) >> 7)
-    let exponent = exponentHigh | exponentLow
-
-    // Mantissa needs to be calculated. Usually this is done with a sum of decimal bit values
-    let mantissaLow = (buffer[offset + i - 2] << 8) | (buffer[offset + i - 3])
-    let mantissaHigh = ((buffer[offset + i - 1] ^ 0x80) << 16)
-    let mantissa = mantissaHigh | mantissaLow
-    // mantissa = (mantissa << 24) | 1
-    // mantissa = mantissa | (1 << 24)
-
-    console.log({offset, i, sign, exponent, mantissaLow, mantissaHigh, mantissa})
-    return sign * Math.pow(2, exponent) * mantissa
-  }
-
-  readComplex(buffer, offset) {
-    // Note: This will return an array:
-    // Index 0 is the Real part
-    // Index 1 is the Imaginary part
-
-    let nBytes = 8; // number of bytes in a complex point value
-
-    // Note: offset is offset of COMPLEX values in the buffer. Not bytes!
-    offset = offset * nBytes
-
-    return [this.readFloat(buffer, offset), this.readFloat(buffer, offset+1)]
-  }
-
-  countFloats(buffer) {
-    return buffer.length/4
-  }
-
-  countComplex(buffer) {
-    return buffer.length/8
-  }
 
   startListening() {
     if (!server || server._receiving !== true) {
@@ -196,47 +108,30 @@ export default class SDR extends Component {
   }
 
   updateCanvas() {
-    // Test the float and complex data parser APIs:
-    // let buffer = [0x00, 0x00, 0x80, 191, 0, 0, 128, 63]
-    // console.log('float count:', this.countFloats(buffer))
-    // console.log('testing readFloat:', this.readFloat(buffer, 0), this.readFloat(buffer, 1))
 
-    // // Treat that same buffer as a complex value:
-    // console.log('complex count:', this.countComplex(buffer))
-    // console.log('readComplex from buffer:', this.readComplex(buffer, 0))
-
-
-    const canvas = this.refs.canvas
-    canvas.width  = window.innerWidth - 16
-    canvas.height = window.innerHeight - 16
-    const ctx = canvas.getContext('2d')
-    ctx.fillStyle = 'rgb(200, 0, 0)'
-    ctx.fillRect(0, 0, 1, 1024)
-    ctx.fillRect(255, 0, 1, 1024)
+    this.ctx.fillStyle = 'rgb(200, 0, 0)'
+    this.ctx.fillRect(0, 0, 1, 1024)
+    this.ctx.fillRect(255, 0, 1, 1024)
 
     if (this.state.rfData.length > 0) {
-      ctx.fillStyle = 'rgb(0, 0, 0)'
+      this.ctx.fillStyle = 'rgb(0, 0, 0)'
       const cutoff = Math.min(this.state.rfData.length, 1024)
-      ctx.beginPath()
-      ctx.moveTo(0, 0)
-      console.log('0th float:', this.readFloat(this.state.rfData, 0))
-      for (let i = 0; i < 4; i+=1) {
-        console.log('data:', i, this.state.rfData[i])
-      }
-      // this.state.rfData.forEach((value, i) => {
-      //   console.log('data:', i, value)
-      // })
-      // for (let i = 0; i < cutoff/4; i+=1) {
-      // //   // ctx.fillRect(this.state.rfData[i], i, 1, 1)
-      // //   if (i < 4) {
-      //     console.log('data:', i, this.state.rfData[i])
-      // //   }
-      // //   ctx.lineTo(this.state.rfData[i],i);
+      this.ctx.beginPath()
+      // this.ctx.moveTo(0, 0)
 
-      // }
-      console.log('----')
-      ctx.strokeStyle = 'rgb(255,255,255)'
-      ctx.stroke()
+      this.state.rfData.forEach((value, i) => {
+        // this.ctx.fillRect(this.state.rfData[i], i, 1, 1)
+        currentYPos += 1
+        if (currentYPos > 512) {
+          currentYPos = 0
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+          this.ctx.fillRect(0, 0, 1, 1024)
+          this.ctx.fillRect(255, 0, 1, 1024)
+        }
+        this.ctx.lineTo(this.state.rfData[i]+127, currentYPos);
+      })
+      this.ctx.strokeStyle = 'rgb(255,255,255)'
+      this.ctx.stroke()
     }
   }
 
